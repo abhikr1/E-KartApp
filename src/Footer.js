392f121c7c9ec79bc83235c6034bd1b46c620533
Abhinav <abhikrofficial@gmail.com>
2021-04-07 00:42:29 +0530

    Removed Unnecessary logs

--- src/Footer.js
+++ src/Footer.js
@@ -9,9 +9,9 @@ return(
     <div class="footer-inside">
       <div class="footer-info">
          <div class="footer-logo" >
-           <a href="https://www.linkedin.com/in/subham-mitra/" target="_blank"><img src="/images/linkedin.svg"></img></a>
-           <a class="middle-footer-logo"href="https://github.com/subham2107" target="_blank"><img src="/images/github.svg"></img></a>
-           <a href="https://twitter.com/subham2107" target="_blank"><img src="/images/twitter.svg"></img></a>
+           <a href="https://www.linkedin.com/in/abhinav-kumar-27803513b/" target="_blank"><img src="/images/linkedin.svg"></img></a>
+           <a class="middle-footer-logo"href="https://github.com/abhikr1" target="_blank"><img src="/images/github.svg"></img></a>
+           <a href="https://twitter.com/Waz33r  " target="_blank"><img src="/images/twitter.svg"></img></a>
           </div>
       </div>
     </div>
